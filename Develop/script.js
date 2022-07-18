aa540f5bd2f3aaa0b9cb4b1d092cba8c676ed796
joel57 <67278919+joel57@users.noreply.github.com>
2022-07-18 15:46:28 -0700

    added functions for the uppercasecheck and fixed bug

--- Develop/script.js
+++ Develop/script.js
@@ -16,17 +16,47 @@ function determineLegth(){
 
   if (passwordlegngth<8) {
     alert("Password length must be a number between 8-128 characters");
+    determineLength();
+  }else if (passwordlength>128){
+    alert("Password length must be a number between 8-128");
+    determineLength();
+  }else if(isNaN(passwordLength)){
+    alert("Password length must be a number 8-128 characters");
+    determineLength();
+  }else{
+    alert("The next three prompts will ask what type of characters you would like to be in the password. If you choose 'No' for AbortController, your password will only contain lower case");
   }
+  return passwordLength;
+
+}
+
+// Creating Functions for lowecase & upcase letters in password.
+function determineUpperCase(){
+  upperCaseCheck = prompt("Do you want to include uppercase leters? Yes or No");
+  upperCaseCheck = upperCaseCheck.toLowerCase();
+
+  if (upperCaseCheck === null || upperCaseCheck === ""){
+    alert ("Please answer Yes or No");
+    ditermineUpperCase();
+
+  }else if (upperCaseCheck === "yes" || upperCaseCheck === "YES"){
+    upperCaseCheck = true;
+return upperCaseCheck;
+
+  }else if (upperCaseCheck === "no" || upperCaseCheck ==="n"){
+    upperCaseCheck = false;
+    return upperCaseCheck;
+  }else {
+    alert("please answer yes or no");
+    determineUpperCase();
+  }
+
+  return upperCaseCheck;
 
 }
 
 
-
-
-//me:
-
-
-
+console.log()
 
 
 
@@ -40,4 +70,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
